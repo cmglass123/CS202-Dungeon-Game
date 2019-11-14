dfca89f9ab2cc972fe1ef69eb43f044c28eb3cbe
Rose Peters <rmpeters3@alaska.edu>
2019-11-14 10:57:51 -0900

    changed color

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	//sf::SquareShape shape(100.f);
-	shape.setFillColor(sf::Color::White);
+	shape.setFillColor(sf::Color::Blue);
 
 	while (window.isOpen())
 	{
