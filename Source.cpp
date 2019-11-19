179afc231b1b012dc3f2fb58acf10199156b7f76
cmglass <cmglass@alaska.edu>
2019-11-19 10:37:28 -0900

    test

--- Source.cpp
+++ Source.cpp
@@ -4,8 +4,8 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
-	//sf::SquareShape shape(100.f);
 	shape.setFillColor(sf::Color::Blue);
+
 
 	while (window.isOpen())
 	{
