ad76160b2cb139e22bfb482cc9bf1ef136bde7fd
Rose Peters <rmpeters3@alaska.edu>
2019-11-25 11:20:30 -0900

    variable names

--- Minesweeper Nov 19/Minesweeper Nov 19.cpp
+++ Minesweeper Nov 19/Minesweeper Nov 19.cpp
@@ -7,7 +7,7 @@ int main()
 
 	sf::RenderWindow app(sf::VideoMode(400, 400), "Minesweeper!");
 
-	
+
 	int imgSize = 32;
 	int grid[12][12];
 	int showGrid[12][12]; //for showing blank tiles
@@ -16,36 +16,36 @@ int main()
 	texture.loadFromFile("tiles.jpg");
 	sf::Sprite sprite(texture);
 
-	for (int i = 1; i <= 10; i++)
-		for (int j = 1; j <= 10; j++)
+	for (int col = 1; col <= 10; col++)
+		for (int row = 1; row <= 10; row++)
 		{
-			
-			showGrid[i][j] = 10;
-			if (rand() % 5 == 0)  grid[i][j] = 9;
-			else grid[i][j] = 0;
+
+			showGrid[col][row] = 10;
+			if (rand() % 5 == 0)  grid[col][row] = 9;
+			else grid[col][row] = 0;
 		}
 
-	for (int i = 1; i <= 10; i++)
-		for (int j = 1; j <= 10; j++)
+	for (int col = 1; col <= 10; col++)
+		for (int row = 1; row <= 10; row++)
 		{
 			int n = 0;
-			if (grid[i][j] == 9) continue;
-			if (grid[i + 1][j] == 9) n++;
-			if (grid[i][j + 1] == 9) n++;
-			if (grid[i - 1][j] == 9) n++;
-			if (grid[i][j - 1] == 9) n++;
-			if (grid[i + 1][j + 1] == 9) n++;
-			if (grid[i - 1][j - 1] == 9) n++;
-			if (grid[i - 1][j + 1] == 9) n++;
-			if (grid[i + 1][j - 1] == 9) n++;
-			grid[i][j] = n;
+			if (grid[col][row] == 9) continue;
+			if (grid[col + 1][row] == 9) n++;
+			if (grid[col][row + 1] == 9) n++;
+			if (grid[col - 1][row] == 9) n++;
+			if (grid[col][row - 1] == 9) n++;
+			if (grid[col + 1][row + 1] == 9) n++;
+			if (grid[col - 1][row - 1] == 9) n++;
+			if (grid[col - 1][row + 1] == 9) n++;
+			if (grid[col + 1][row - 1] == 9) n++;
+			grid[col][row] = n;
 		}
 
 	while (app.isOpen())
 	{
 		sf::Vector2i pos = sf::Mouse::getPosition(app);
-		int x = pos.x / imgSize;
-		int y = pos.y / imgSize;
+		int mouseX = pos.x / imgSize;
+		int mouseY = pos.y / imgSize;
 
 		sf::Event event;
 		while (app.pollEvent(event))
@@ -54,20 +54,23 @@ int main()
 				app.close();
 
 			if (event.type == sf::Event::MouseButtonPressed)
-				if (event.key.code == sf::Mouse::Left) showGrid[x][y] = grid[x][y];
-				else if (event.key.code == sf::Mouse::Right) showGrid[x][y] = 11;
+				if (event.key.code == sf::Mouse::Left) showGrid[mouseX][mouseY] = grid[mouseX][mouseY];
+				else if (event.key.code == sf::Mouse::Right) showGrid[mouseX][mouseY] = 11;
 		}
 
 		//color of background
 		app.clear(sf::Color::White);
+
 		//add column to grid
-		for (int i = 1; i <= 10; i++)
+		for (int col = 1; col <= 10; col++)
 			//add row to grid
-			for (int j = 1; j <= 10; j++)
+			for (int row = 1; row <= 10; row++)
 			{
-				if (showGrid[x][y] == 9) showGrid[i][j] = grid[i][j];
-				sprite.setTextureRect(sf::IntRect(showGrid[i][j] * imgSize, 0, imgSize, imgSize));
-				sprite.setPosition(i * imgSize, j * imgSize);
+				//show tiles if bomb tile is pressed
+				if (showGrid[mouseX][mouseY] == 9) showGrid[col][row] = grid[col][row];
+
+				sprite.setTextureRect(sf::IntRect(showGrid[col][row] * imgSize, 0, imgSize, imgSize));
+				sprite.setPosition(col * imgSize, row * imgSize);
 				app.draw(sprite);
 			}
 
