c66c3d708ebac413553aa8bf59e9e362cd988d26
Matt Andrews <wmandrews@alaska.edu>
2019-11-26 11:31:04 -0900

    Revert "created new tiles image, added additional tile position, (treasure chest) which is now referenced in position 12, refactored to make code more intelligible."
    This reverts commit d3a76401bb5164c1ccba36da55fa5ffe03bf02cb.

--- Minesweeper Nov 19/Minesweeper Nov 19.cpp
+++ Minesweeper Nov 19/Minesweeper Nov 19.cpp
@@ -10,15 +10,16 @@ int main()
 
 	int w = 32;
 	int grid[12][12];
-	int showGrid[22][22]; //for showing
+	int sgrid[12][12]; //for showing
 
 	Texture t;
-	t.loadFromFile("tiles2.jpg");
+	t.loadFromFile("tiles.jpg");
 	Sprite s(t);
+
 	for (int i = 1; i <= 10; i++)
 		for (int j = 1; j <= 10; j++)
 		{
-			showGrid[i][j] = 10;
+			sgrid[i][j] = 10;
 			if (rand() % 5 == 0)  grid[i][j] = 9;
 			else grid[i][j] = 0;
 		}
@@ -42,31 +43,26 @@ int main()
 	while (app.isOpen())
 	{
 		Vector2i pos = Mouse::getPosition(app);
-		int mouseXPos = pos.x / w;
-		int mouseYPos = pos.y / w;
+		int x = pos.x / w;
+		int y = pos.y / w;
 
-		Event somethingHasHappened;
-		while (app.pollEvent(somethingHasHappened))
+		Event e;
+		while (app.pollEvent(e))
 		{
-			if (somethingHasHappened.type == Event::Closed)
+			if (e.type == Event::Closed)
 				app.close();
 
-			if (somethingHasHappened.type == Event::MouseButtonPressed)
-			{
-				if (somethingHasHappened.key.code == Mouse::Left)
-				{
-					showGrid[mouseXPos][mouseYPos] = grid[mouseXPos][mouseYPos];
-				}
-				else if (somethingHasHappened.key.code == Mouse::Right) showGrid[mouseXPos][mouseYPos] = 12;
-			}
+			if (e.type == Event::MouseButtonPressed)
+				if (e.key.code == Mouse::Left) sgrid[x][y] = grid[x][y];
+				else if (e.key.code == Mouse::Right) sgrid[x][y] = 11;
 		}
 
 		app.clear(Color::White);
 		for (int i = 1; i <= 10; i++)
 			for (int j = 1; j <= 10; j++)
 			{
-				if (showGrid[mouseXPos][mouseYPos] == 9) showGrid[i][j] = grid[i][j];
-				s.setTextureRect(IntRect(showGrid[i][j] * w, 0, w, w));
+				if (sgrid[x][y] == 9) sgrid[i][j] = grid[i][j];
+				s.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
 				s.setPosition(i * w, j * w);
 				app.draw(s);
 			}
